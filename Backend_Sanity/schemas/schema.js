5f0839366a1213d081370b05445482012d0442c3
gagankumar03 <gagankumar03@nagarro.com>
2022-10-04 15:57:59 +0530

    'complete'

--- Backend_Sanity/schemas/schema.js
+++ Backend_Sanity/schemas/schema.js
@@ -4,6 +4,14 @@ import createSchema from "part:@sanity/base/schema-creator";
 // Then import schema types from any plugins that might expose them
 import schemaTypes from "all:part:@sanity/base/schema-type";
 import testimonials from "./testimonials";
+import Abouts from "./Abouts";
+import Works from "./Works";
+import brands from "./brands";
+import contact from "./contact";
+import experiences from "./experiences";
+import workExperience from "./workExperience";
+import Skills from "./Skills";
+
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
   // We name our schema
@@ -12,5 +20,12 @@ export default createSchema({
   types: schemaTypes.concat([
     /* Your types here! */
     testimonials,
+    Abouts,
+    Works,
+    Skills,
+    brands,
+    contact,
+    experiences,
+    workExperience,
   ]),
 });
